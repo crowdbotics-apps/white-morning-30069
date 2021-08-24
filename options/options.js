84219c809a46a84f85b08365fe152c72b3587768
Crowdbotics <team@crowdbotics.com>
2021-08-24 14:02:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "white_morning_30069Identifier",
+  url: "https://white_morning_30069Identifier.botics.co",
+  api: "https://white_morning_30069Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
